5584f8ad86b322f177ddb9053b270806c78d7365
berryberry189 <58412521+berryberry189@users.noreply.github.com>
2021-06-06 23:12:07 +0900

    add Member

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+
+}
