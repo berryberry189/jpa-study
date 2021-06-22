a0bd34b3e72c4ca3cb0f15f4f3ae7e5774245d49
berryberry189 <berryberry189@naver.com>
2021-06-22 16:43:49 +0900

    주문 검색 기능 개발 ing add OrderSearch modi OrderRepository, OrderServiceTest

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -14,10 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
