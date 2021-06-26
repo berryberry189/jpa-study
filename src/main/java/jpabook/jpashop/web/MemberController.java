0666ee2731729804b53f5829dcce3a93c4e4fdd6
berryberry189 <58412521+berryberry189@users.noreply.github.com>
2021-06-26 15:00:46 +0900

    회원 목록 조회 add memberList.html modi MemberController.java

--- src/main/java/jpabook/jpashop/web/MemberController.java
+++ src/main/java/jpabook/jpashop/web/MemberController.java
@@ -19,6 +19,13 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @GetMapping(value = "/members")
+    public String  list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
     @GetMapping(value = "/members/new")
     public String createForm(Model model){
         model.addAttribute("memberForm", new MemberForm());
