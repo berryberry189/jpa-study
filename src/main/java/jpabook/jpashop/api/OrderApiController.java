329aa13436a6d43792955af0b98e5c2bda88c61d
berryberry189 <berryberry189@naver.com>
2021-07-23 18:03:25 +0900

    주문조회 v4 : jpa에서 dto직접 조회 - dto안에 리스트 있는 경우 쿼리 2개 생성

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.*;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,7 @@ import java.util.stream.Collectors;
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1(){
@@ -62,6 +65,11 @@ public class OrderApiController {
                 .collect(Collectors.toList());
 
         return result;
+    }
+
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4(){
+        return orderQueryRepository.findOrderQueryDtos();
     }
 
     @Getter
